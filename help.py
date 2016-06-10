54913672c3bb9634d251d2e8db8985dc71d78683
kenoung <E0003273@u.nus.edu>
2016-06-10 11:47:43 +0800

    added templates

--- help.py
+++ help.py
@@ -1,15 +1,43 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, session, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
+from api import *
 import requests
 import json
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 's0m3s3cre+'
+app.config['SECRET_KEY'] = 'hard2guess'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+##########
+# Models #
+##########
+
+class User(db.Model):
+    __tablename__ = 'users'
+    user_id = db.Column(db.String(20), primary_key=True)
+    name = db.Column(db.String(64))
+    
+    def __repr__(self):
+        return '<User {id}: {name}>'.format(id=self.user_id, name=self.name)
+
+class Module(db.Model):
+	
+##########
+# Routes #
+##########
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -17,10 +45,22 @@ def index():
 
 @app.route('/inside')
 def inside():
-    token=request.args['token']
-    r = requests.get('https://ivle.nus.edu.sg/api/Lapi.svc/Profile_View?APIKey={APIKey}&AuthToken={AuthToken}'.format(APIKey="UY5RaT4yK3lgWflM47CJo", AuthToken=token))
-    name = r.json()['Results'][0]['Name'].title()
-    return render_template('inside.html', token=token, name=name)
+	if request.args.get('token'):
+		session['token'] = request.args['token']
+	elif not logged_in(session.get('token')):
+		flash("You are currently logged out. Please log in.")
+		return redirect(url_for('index'))
+
+	name = get_name(session.get('token'))
+	return render_template('inside.html', name=name.title())
+
+@app.route('/get_help')
+def get_help():
+	if not logged_in(session.get('token')):
+		flash("You are currently logged out. Please log in.")
+		return redirect(url_for('index'))
+	return render_template('get_help.html')
+
 
 # Error Handling
 # @app.errorhandler(404)
