1636ec8d558b7cc60ebc9976c806e0ca702cbdaf
kenoung <E0003273@u.nus.edu>
2016-06-25 21:49:55 +0800

    fixed logout path and quit button síze

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,11 +1,11 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from . import main
 
 # Error Handling
 @main.app_errorhandler(401)
 def logged_out(e):
     flash("You are currently logged out. Please log in.")
-    return redirect(url_for('index'))
+    return redirect(url_for('.index'))
 
 @main.app_errorhandler(404)
 def page_not_found(e):
